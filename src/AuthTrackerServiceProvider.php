3dc3783a7f0d1e4a0a720154713052e61588dbf9
@mastersofthouse <contato@mastersofthouse.com.br>
2022-09-11 17:21:43 -0300

    atualização pack

--- src/AuthTrackerServiceProvider.php
+++ src/AuthTrackerServiceProvider.php
@@ -17,11 +17,6 @@ class AuthTrackerServiceProvider extends ServiceProvider
                 __DIR__.'/../config/authtracker.php' => config_path('authtracker.php'),
             ], 'config');
 
-            $this->publishes([
-                __DIR__ . '/../database/migrations/update_personal_access_tokens_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . 'update_personal_access_tokens_table.php'),
-                // you can add any number of migrations here
-              ], 'migrations');
-
         }
 
         Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
